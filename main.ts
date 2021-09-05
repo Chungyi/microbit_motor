94b6afc9e215aec54a9f72ad53b6d604cc4e2722
Chung-Yi Huang <chungyi410@gmail.com>
2021-09-05 22:01:30 +0800

    1.啟動時，停止所有電機，舵機S8角度為0。2.按A按鈕，舵機S8角度為30，電機M2正轉速度100。3.按B按鈕，舵機S8角度為150，電機M2正轉速度150。

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    motor.servo(motor.Servos.S8, 30)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
+})
+input.onButtonPressed(Button.B, function () {
+    motor.servo(motor.Servos.S8, 150)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 150)
+})
+motor.motorStopAll()
+motor.servo(motor.Servos.S8, 0)
