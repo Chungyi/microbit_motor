42be2b64de87fc6917145dbfbf7556b288a59fc1
Chung-Yi Huang <chungyi410@gmail.com>
2021-09-05 22:07:22 +0800

    新增M1馬達。

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     motor.servo(motor.Servos.S8, 30)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 80)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 100)
 })
 input.onButtonPressed(Button.B, function () {
     motor.servo(motor.Servos.S8, 150)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 100)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 150)
 })
 motor.motorStopAll()
